e3c305f28dcef25f5dd41b9eb372af685ceebf3d
pkmaneri <praveen.kumar.maneri@gmail.com>
2021-11-02 21:25:05 +0530

    fc

--- app.js
+++ app.js
@@ -14,14 +14,10 @@ var db = 'mongodb://localhost:27017/mss_db'
 
 
 if (process.env.NODE_ENV === 'production') {
-app.use(express(client/build))
- 
-}
+  app.use(express.static("client/build"))
+  }
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
-  app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 console.log("mongo => ", db)
 mongoose.Promise = global.Promise;
@@ -76,16 +72,46 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
 
 
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const path = require('path');
+
+// const items = require('./routes/api/items');
+
+// const app = express();
+
+// app.use(bodyParser.json());
+
+// const db = require('./config/keys').mongoURI;
+// mongoose
+//  .connect(db)
+//  .then(() => console.log('MongoDB Connected...'))
+//  .catch(err => console.log(err));
+
+
+//  app.use('/api/items', items);
+
+//  if (process.env.NODE_ENV === 'production') {
+//      app.use(express.static('client/build'));
+
+//      app.get('*', (req, res) => {
+//          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+//  }
+
+//  const port = process.env.PORT || 5000;
+//  app.listen(port, () => console.log(`Server started on port ${port}`));
