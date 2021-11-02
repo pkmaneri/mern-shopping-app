abaf9eccb3249993f3e99a4cc162607859003af5
pkmaneri <praveen.kumar.maneri@gmail.com>
2021-11-02 21:37:10 +0530

    fc

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
